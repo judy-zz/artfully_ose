406b265e62e68ecab9d243f92875f5acef87498a
Micah Frost <micah.frost@fracturedatlas.org>
2010-09-27 10:39:18 -0400

    New Rails3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtfulLy::Application.config.secret_token = 'cb23c248e119e4778f463f87c65e4c38eb0606ab2e25335d938c511c17fc7589173fa092b632a40a991bca804707acfa5bbb9d96794849a235a758dd37333c35'
