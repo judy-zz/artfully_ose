41068f329c3db45e7225e25350ba179e139b785c
Micah Frost <micah.frost@fracturedatlas.org>
2010-11-01 18:04:49 -0400

    TicketProxy now has method missing for class methods as well

--- app/models/ticket_proxy.rb
+++ app/models/ticket_proxy.rb
@@ -6,6 +6,10 @@ class TicketProxy
     @id = id
   end
 
+  def self.method_missing(name, *args, &block)
+    Ticket.send(name, *args, &block)
+  end
+
   def method_missing(name, *args, &block)
     @ticket ||= Ticket.find(@id)
     @ticket.send(name, *args, &block)
