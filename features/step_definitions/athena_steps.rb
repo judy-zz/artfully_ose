61409782e4da286d81adbf93421efa80c02c8282
Gary Moore <gary.moore@fracturedatlas.org>
2010-11-10 16:40:50 -0500

    Some testing etc

--- features/step_definitions/athena_steps.rb
+++ features/step_definitions/athena_steps.rb
@@ -10,7 +10,19 @@ Given /^I can get Tickets from ATHENA$/ do
   FakeWeb.register_uri(:get, %r|http://localhost/tickets/\.json|, :status => [ 200 ], :body => "[]")
 end
 
+Given /^I have found the following tickets for purchase$/ do |tickets|
+  body = []
+  tickets.each do |ticket|
+    body << Factory(:ticket, {:id=>ticket['id'], :venue=>ticket['venue'], :event=>ticket['event'], :performance=>['performance']})
+  end
+  FakeWeb.register_uri(:get, %r|http://localhost/tickets/\?.*$|, :status => [ 200 ], :body => body.encode)
+  visit tickets_path
+  fill_in("Performance", :with => tickets.first['performance'])
+  click_button("Search")
+end
+
 Then /^the last request to ATHENA should include "([^"]*)"$/ do |exp|
   FakeWeb.last_request =~ exp
 end
+
 
