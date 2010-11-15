264c39a8cec3b46a6c83d6d42536755b0280b482
Micah Frost <micahfrost@fracturedatlas.org>
2010-11-15 14:16:01 -0500

    CRUD a credit card

--- app/models/athena/credit_card.rb
+++ app/models/athena/credit_card.rb
@@ -2,17 +2,42 @@ class Athena::CreditCard < AthenaResource::Base
 
   self.site = Artfully::Application.config.tickets_site
 
+
+  self.prefix = '/payments/'
+  self.collection_name = 'cards'
+  self.element_name = 'cards'
+
   schema do
-    attribute 'cardholder_name',        :string
-    attribute 'card_number',            :string
-    attribute 'expiration_date',        :string
-    attribute 'cvv',                    :string
+    attribute 'cardholderName',        :string
+    attribute 'cardNumber',            :string
+    attribute 'expirationDate',        :string
+    attribute 'cvv',                   :string
+
+    attribute 'customer', :string
   end
+
+
+  # Note: This is used to provide a more ruby-friendly set of accessors that will still serialize properly.
+  def self.aliased_attr_accessor(*accessors)
+    attr_reader :attributes
+    accessors.each do |attr|
+      attr = attr.to_s.camelize(:lower)
+      class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
+        def #{attr}()                     @attributes['#{attr}'] end
+        def #{attr}=(#{attr})             @attributes['#{attr}'] = #{attr} end
+        def #{attr.underscore}()          @attributes['#{attr}'] end
+        def #{attr.underscore}=(#{attr})  @attributes['#{attr}'] = #{attr} end
+      RUBY_EVAL
+    end
+  end
+
+
+  aliased_attr_accessor :card_number, :expiration_date, :cardholder_name, :cvv
 
   validates_presence_of :card_number, :expiration_date, :cardholder_name, :cvv
 
   def initialize(attrs = {})
-    prepare_attr!(attrs) unless attrs.has_key? :expiration_date
+    prepare_attr!(attrs) if needs_date_parse(attrs)
     super
   end
 
@@ -21,6 +46,10 @@ class Athena::CreditCard < AthenaResource::Base
   end
 
   private
+    def needs_date_parse(attrs)
+      !attrs.empty? && ( attrs.has_key? :expiration_date or attrs.has_key? :expirationDate )
+    end
+
     def prepare_attr!(attributes)
       unless attributes.empty?
         day = attributes.delete('expiration_date(3i)')
--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -12,4 +12,51 @@ describe Athena::CreditCard do
     @card = Factory(:credit_card)
     @card.to_json.should match(/\"expirationDate\":\"#{@card.expiration_date.strftime('%m\/%Y')}\"/)
   end
+
+  describe "#encode" do
+    subject { Factory(:credit_card).encode }
+    %w( cardNumber expirationDate cardholderName cvv ).each do |attribute|
+      it { should match(attribute) }
+    end
+  end
+
+  describe "#find" do
+    it "should find the card by id" do
+      FakeWeb.register_uri(:get, "http://localhost/payments/cards/1.json", :status => "200", :body => Factory(:credit_card, :id => 1).encode)
+      @card = Athena::CreditCard.find(1)
+
+      FakeWeb.last_request.method.should == "GET"
+      FakeWeb.last_request.path.should == "/payments/cards/1.json"
+    end
+  end
+
+  describe "#save" do
+    it "should issue a PUT when updating a card" do
+      @card = Factory(:credit_card, :id => "1")
+      FakeWeb.register_uri(:put, "http://localhost/payments/cards/#{@card.id}.json", :status => "200")
+      @card.save
+
+      FakeWeb.last_request.method.should == "PUT"
+      FakeWeb.last_request.path.should == "/payments/cards/#{@card.id}.json"
+    end
+
+    it "should issue a POST when creating a new Athena::Ticket" do
+      FakeWeb.register_uri(:post, "http://localhost/payments/cards/.json", :status => "200")
+      @card = Factory.create(:credit_card)
+
+      FakeWeb.last_request.method.should == "POST"
+      FakeWeb.last_request.path.should == "/payments/cards/.json"
+    end
+  end
+
+  describe "#destroy" do
+    it "should issue a DELETE when destroying a card" do
+      @card = Factory(:credit_card, :id => "1")
+      FakeWeb.register_uri(:delete, "http://localhost/payments/cards/#{@card.id}.json", :status => "204")
+      @card.destroy
+
+      FakeWeb.last_request.method.should == "DELETE"
+      FakeWeb.last_request.path.should == "/payments/cards/#{@card.id}.json"
+    end
+  end
 end
