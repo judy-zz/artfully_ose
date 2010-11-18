3d39c2c6a5dcb8aa63cb6cc31a92aa5a13a57c0b
Micah Frost <micah@fracturedatlas.org>
2010-11-18 00:56:19 -0500

    Fixed truncation issue with database_cleaner and seeded roles

--- features/support/env.rb
+++ features/support/env.rb
@@ -54,7 +54,7 @@ Cucumber::Rails::World.use_transactional_fixtures = false
 if defined?(ActiveRecord::Base)
   begin
     require 'database_cleaner'
-    DatabaseCleaner.strategy = :truncation, { :except => %w[ :roles ] }
+    DatabaseCleaner.strategy = :truncation, { :except => %w[ roles ] }
   rescue LoadError => ignore_if_database_cleaner_not_present
   end
 end
