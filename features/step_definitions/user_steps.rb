2a2669efc2465b3b657b040445ead7d04bca18c9
Micah Frost <micah@fracturedatlas.org>
2010-11-22 09:36:09 -0500

    Removed user step that can be done with FG

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -7,10 +7,6 @@ Given /^I am logged in(?: as a "([^"]*)")?$/ do |role|
   click_button("Sign in")
 end
 
-Given /^the following users:$/ do |users|
-  User.create!(users.hashes)
-end
-
 When /^I delete the (\d+)(?:st|nd|rd|th) user$/ do |pos|
   visit users_path
   within("table tr:nth-child(#{pos.to_i+1})") do
