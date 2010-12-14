f80917812128805edfc86e377a70a4823d06bcda
Micah Frost <micah@fracturedatlas.org>
2010-12-14 16:12:09 -0500

    Added multi-item add to order cuke

--- features/step_definitions/ticket_steps.rb
+++ features/step_definitions/ticket_steps.rb
@@ -1,4 +1,5 @@
 Given /^I have found (\d+) tickets to "([^"]*)" at "([^"]*)" for \$(\d+)$/ do |quantity, event, venue, price|
+  Given %{I am on the tickets page}
   body = []
   ids = []
   quantity.to_i.times do
@@ -10,6 +11,22 @@ Given /^I have found (\d+) tickets to "([^"]*)" at "([^"]*)" for \$(\d+)$/ do |q
   FakeWeb.register_uri(:any, %r|http://localhost/tix/meta/locks/.*\.json|, :status => [ 200 ], :body => Factory(:unexpired_lock, :tickets => ids).encode)
 
   Given %Q{I search for #{quantity} tickets for $#{price}}
+end
+
+Given /^I have added (\d+) tickets to "([^"]*)" at "([^"]*)" for \$(\d+)$/ do |quantity, event, venue, price|
+  Given %{I have found #{quantity} tickets to "#{event}" at "#{venue}" for $#{price}}
+  Given %{I press "Add Tickets"}
+end
+
+Then /^I should see (\d+) tickets to "([^"]*)" at "([^"]*)" for \$(\d+)$/ do |quantity, event, venue, price|
+  Then %{I should see "#{event}"}
+  Then %{I should see "#{venue}"}
+  Then %{I should see "#{price}"}
+  Then %{I should see #{quantity} tickets}
+end
+
+Then /^I should see (\d+) tickets$/ do |quantity|
+  page.has_xpath? "//li[@class='ticket']", :count => 2
 end
 
 Given /^I search for (\d+) tickets for \$(\d+)$/ do |quantity, price|
