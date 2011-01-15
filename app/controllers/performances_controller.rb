d3b58dc88b6e0b0ffac32a677af951776ff8d538
Gary Moore <gary.moore@fracturedatlas.org>
2011-01-14 19:56:08 -0500

    More performancing

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -55,34 +55,9 @@ class PerformancesController < ApplicationController
   def update
     @performance = AthenaPerformance.find(params[:id])
     @bulk_action = params[:bulk_action]
-    #if this is a bulk edit tickets action...
+    
     if(!@bulk_action.nil?)
-      @selected_tickets = params[:selected_tickets]
-      
-      if @selected_tickets.nil?
-        flash[:error] = "You didn't select any tickets"
-      else
-      
-        @msg = ''
-        @selected_tickets.each do |ticket_id|
-          @ticket = AthenaTicket.find(ticket_id)
-          case @bulk_action
-            when "PUT_ON_SALE"
-              @ticket.on_sale=true
-              @ticket.save
-            when 'TAKE_OFF_SALE'
-              @ticket.on_sale=false
-              @ticket.save
-            when 'DELETE'
-              @ticket.destroy
-            else
-              @msg += ' bonk? '
-          end
-          @msg += ticket_id + ' '
-        end
-  
-        flash[:notice] = "[" + params[:bulk_action] + "]"  + ' ' + @msg
-      end
+      bulk_edit_tickets params[:selected_tickets], @bulk_action 
       redirect_to performance_url(@performance) and return
     else
       without_tickets do
@@ -121,5 +96,40 @@ class PerformancesController < ApplicationController
       else
         yield
       end
+    end
+    
+    def bulk_edit_tickets ticket_ids, action  
+      if ticket_ids.nil?
+        flash[:error] = "You didn't select any tickets"
+        return
+      else
+        ticket_ids.each do |ticket_id|
+          @ticket = AthenaTicket.find(ticket_id)
+          
+          #TODO: Put this logic into @performance model?
+          case action
+            when "PUT_ON_SALE"
+              @ticket.on_sale=true
+              @ticket.save
+            when 'TAKE_OFF_SALE'
+              @ticket.on_sale=false
+              @ticket.save
+            when 'DELETE'
+              @ticket.destroy
+          end        
+        
+          case action
+            when "PUT_ON_SALE"
+              @msg = "Put " + ticket_ids.size.to_s + " tickets on sale"
+            when 'TAKE_OFF_SALE'
+              @msg = "Took " + ticket_ids.size.to_s + " tickets off sale"
+            when 'DELETE'
+              @msg = "Deleted " + ticket_ids.size.to_s + " tickets "
+            else
+              @msg = "Please select some tickets"
+          end
+        end
+      end
+      flash[:notice] = @msg
     end
 end
