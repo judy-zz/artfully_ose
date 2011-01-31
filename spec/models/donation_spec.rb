1ae212197a25104f426548ae2eccf8b9a2e40aa6
Micah Frost <micah.frost@fracturedatlas.org>
2011-01-31 16:51:53 -0500

    Initial refactoring to support Donations in the Order

---
+++ spec/models/donation_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe Donation do
+  subject { Factory(:donation) }
+
+  it { should be_valid }
+
+  it { should respond_to :amount }
+  it { should respond_to :order }
+  it { should respond_to :recipient }
+  it { should respond_to :recipient= }
+
+  describe ".amount" do
+    it "should not be valid without an amount" do
+      subject.amount = nil
+      subject.should_not be_valid
+    end
+
+    it "should not be valid with an amount less than 0" do
+      subject.amount = -1
+      subject.should_not be_valid
+    end
+
+    it "should not be valid with an amount equal to 0" do
+      subject.amount = 0
+      subject.should_not be_valid
+    end
+  end
+end
