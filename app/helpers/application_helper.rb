e9816fb8aac081c41b998b4381afa291681165a0
Micah Frost <micah.frost@fracturedatlas.org>
2011-02-14 15:40:33 -0500

    Contextual menu helper and new stylings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   include LinkHelper
   include BulkEditHelper
   include TicketTableHelper
+
+  def contextual_menu(&block)
+    menu = ContextualMenu.new(self)
+    block.call(menu)
+    menu.render_menu
+  end
 end
