751e90e67abbd3a3eac70001a27315f271de4561
Ian Guffy <Ian.Guffy@FracturedAtlas.org>
2011-02-22 15:31:20 -0500

    Memberships in an organization can be added/removed from the admin organization page, users cannot be added to more than one organization.

--- app/controllers/admin/memberships_controller.rb
+++ app/controllers/admin/memberships_controller.rb
@@ -9,35 +9,44 @@ class Admin::MembershipsController < Admin::AdminController
 
   def new
     @organization = Organization.find(params[:organization_id])
+  end
 
+  def create
+    @organization = Organization.find(params[:organization_id])
     authorize! :create, Membership
 
     unless params[:user_email].blank?
       @user = User.find_by_email(params[:user_email])
       if @user.nil?
         flash[:error] = "User #{params[:user_email]} could not be found."
-        redirect_to admin_organization_path(@organization)
+
+      elsif not (mship = Membership.find_by_user_id(@user.id)).nil?
+        if mship.organization_id == @organization.id
+          flash[:alert] = "#{@user.email} is already a member, and was not added a second time."
+        else
+          flash[:error] = "User #{params[:user_email]} is already a member of #{Organization.find(mship.organization_id).name} and cannot be a member of multiple organizations."
+        end
+
       else
         @membership = Membership.new
         @membership.user_id = @user.athena_id
         @membership.organization_id = @organization.id
 
         if @membership.save
-          flash[:notice] = "Your user has been added successfully."
-          redirect_to admin_organization_path(@organization)
+          flash[:notice] = "#{@user.email} has been added successfully."
+        #once a user can be in multiple organizations, this is a better check for duplicate memberships
+        #elsif @membership.errors[:user_id] == ["has already been taken"]
+        #  flash[:alert] = "#{@user.email} is already a member, and was not added a second time."
         else
-          flash[:error] = "Your user has not been added."
-          redirect_to admin_organization_path(@organization)
+          flash[:error] = "User #{@user.email} could not been added."
         end
       end
+      redirect_to admin_organization_url(@organization)
     end
-  end
-
-  def create
+    
   end
 
   def edit
-    #destroy
   end
 
   def update
@@ -48,17 +57,7 @@ class Admin::MembershipsController < Admin::AdminController
     @mship = Membership.find(params[:id])
     authorize! :destroy, @mship
     @mship.destroy
-    redirect_to admin_organization_path(@organization), :notice => "User has been removed from #{@organization.name}" and return
+    redirect_to admin_organization_url(@organization), :notice => "User has been removed from #{@organization.name}" and return
   end
-
-#  def remove_member
-#    @organization = Organization.find(params[:id])
-#
-#    if @organization
-#      @member = @organization.users.find(params[:user_id])
-#      @organization.users.delete(@member)
-#    end
-#    redirect_to admin_organization_path(@organization), :notice => "User #{@member.email} has been removed from #{@organization.name}" and return
-#  end
 
 end
