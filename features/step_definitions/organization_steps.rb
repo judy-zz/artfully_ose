b8f4bfb778726dfb8d2a0f45f80b2d9e081af100
Micah Frost <micah.frost@fracturedatlas.org>
2011-02-25 10:53:50 -0500

    Added admin organization membership cukes

--- features/step_definitions/organization_steps.rb
+++ features/step_definitions/organization_steps.rb
@@ -24,3 +24,29 @@ Then /^I should be a part of the organization "([^"]*)"$/ do |name|
   @current_user.reload
   @current_user.organizations.should include Organization.find_by_name(name)
 end
+
+Given /^"([^"]*)" is part of "([^"]*)"$/ do |email, organization|
+  user = User.find_by_email(email)
+  organization = Organization.find_by_name(organization)
+  organization.users << user
+end
+
+When /^I click the link to remove "([^"]*)"$/ do |email|
+  within(:xpath, "//ul/li[contains(.,'#{email}')]") do
+    click_link "Remove from organization"
+  end
+end
+
+Then /^"([^"]*)" should be a part of "([^"]*)"$/ do |email, organization|
+  user = User.find_by_email(email)
+  organization = Organization.find_by_name(organization)
+  organization.users.should include user
+end
+
+Then /^"([^"]*)" should not be a part of "([^"]*)"$/ do |email, organization|
+  user = User.find_by_email(email)
+  organization = Organization.find_by_name(organization)
+  organization.users.should_not include user
+end
+
+#$x("//ul/li[contains(.,'micah.frost@fracturedatlas.org')]");
