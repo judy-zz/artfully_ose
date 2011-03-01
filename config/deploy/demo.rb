1d282974195dc60870714ce3aadcf49a25fd7b40
Micah Frost <micah.frost@fracturedatlas.org>
2011-03-01 16:26:37 -0500

    Added demo environment, updated cap to use it for EC2

---
+++ config/deploy/demo.rb
@@ -0,0 +1,16 @@
+set :deploy_to, "/var/www/artful.ly"
+set :user, "deploy"
+
+role :web, "184.73.209.105"                          # Your HTTP server, Apache/etc
+role :app, "184.73.209.105"                          # This may be the same as your `Web` server
+role :db,  "184.73.209.105", :primary => true        # This is where Rails migrations will run
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "#{try_sudo} ln -s #{File.join(deploy_to, 'shared', 'db', 'demo.sqlite3')} #{File.join(current_path,'db', 'demo.sqlite3')}"
+   #run "#{try_sudo} cp #{File.join(deploy_to, 'shared', 'config', 'environments', '*')} #{File.join(current_path,'config', 'environments')}"
+  end
+end
