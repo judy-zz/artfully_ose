57635f6b4a3af6bfd53fafc6219f19a26a103b81
Ian Guffy <Ian.Guffy@FracturedAtlas.org>
2011-03-10 14:18:47 -0500

    Bulk actions now throw flash message if no tickets are selected.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -9,11 +9,13 @@ class TicketsController < ApplicationController
     authorize! :bulk_edit, :tickets
     @performance = AthenaPerformance.find(params[:performance_id])
     @selected_tickets = params[:selected_tickets]
-    if 'Comp' == params[:commit]
+    if @selected_tickets.nil?
+      flash[:error] = "No tickets were selected"
+      redirect_to performance_url(@performance) and return
+    elsif 'Comp' == params[:commit]
       redirect_to comp_ticket_people_url(@performance, @selected_tickets) and return #comp_ticket_people#(@performance, params[:selected_tickets], params[:commit])
     else
       with_confirmation do
-        #@performance = AthenaPerformance.find(params[:performance_id])
         bulk_edit_tickets(@performance, params[:selected_tickets], params[:commit])
         redirect_to performance_url(@performance) and return
       end
@@ -70,38 +72,26 @@ class TicketsController < ApplicationController
     end
 
     def bulk_edit_tickets(performance, ticket_ids, action)
-      if ticket_ids.nil?
-        flash[:error] = "You didn't select any tickets"
-        return
-      elsif action.nil?
-        flash[:error] = "You didn't select an action"
-        return
+      rejected_ids = performance.bulk_edit_tickets(ticket_ids, action)
+      edited_tickets = ticket_ids.size - rejected_ids.size
+      case action
+      when "Put on Sale"
+        @msg = "Put #{edited_tickets.to_s} ticket(s) on sale. "
+      when 'Take off Sale'
+        @msg = "Took #{edited_tickets.to_s} ticket(s) off sale. "
+      when 'Delete'
+        @msg = "Deleted #{edited_tickets.to_s} ticket(s). "
       else
-        rejected_ids = performance.bulk_edit_tickets(ticket_ids, action)
-        edited_tickets = ticket_ids.size - rejected_ids.size
-        case action
-          when "Put on Sale"
-            @msg = "Put #{edited_tickets.to_s} ticket(s) on sale. "
-          when 'Take off Sale'
-            @msg = "Took #{edited_tickets.to_s} ticket(s) off sale. "
-          when 'Delete'
-            @msg = "Deleted #{edited_tickets.to_s} ticket(s). "
-          else
-            @msg = "Please select an action. "
-        end
+        @msg = "Please select an action. "
+      end
 
-        if rejected_ids.size > 0
-          @msg += rejected_ids.size.to_s + " ticket(s) could not be edited.
+      if rejected_ids.size > 0
+        @msg += rejected_ids.size.to_s + " ticket(s) could not be edited.
                 Tickets that have been sold or comped can't be put on or taken off sale.
                 A ticket that is already on sale or off sale can't be put on or off sale again."
-          flash[:alert] = @msg
-        else
-          flash[:notice] = @msg
-        end
+        flash[:alert] = @msg
+      else
+        flash[:notice] = @msg
       end
     end
-
-#    def comp_tickets(performance, ticket_ids, action)
-#    end
-
 end
