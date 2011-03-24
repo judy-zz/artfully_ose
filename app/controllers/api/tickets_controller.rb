c331547f47dac4499407e620615273dc44d7cda8
Micah Frost <micah.frost@fracturedatlas.org>
2011-03-24 18:50:48 -0400

    Removed controller parameters from ticket search to avoid ArgumentErrors

--- app/controllers/api/tickets_controller.rb
+++ app/controllers/api/tickets_controller.rb
@@ -1,10 +1,11 @@
 class Api::TicketsController < ApiController
 
   def index
-    @tickets = AthenaTicket.search(params)
-    respond_to do |format|
-      format.json { render :json => @tickets.to_json }
-    end
+    @tickets = AthenaTicket.search(params.reject { |k,v| %w( action controller format callback _ ).include? k })
+    ap @tickets
+#    respond_to do |format|
+#      format.json { render :json => @tickets.to_json }
+#    end
   end
 
 end
