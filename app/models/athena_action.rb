ef93cb28cf3696cb007c7c433d61ae2a7306a9ad
Ian Guffy <Ian.Guffy@FracturedAtlas.org>
2011-03-31 11:34:08 -0400

    Added athena action class for actions to inherit from

---
+++ app/models/athena_action.rb
@@ -0,0 +1,66 @@
+class AthenaAction < AthenaResource::Base
+
+  self.site = Artfully::Application.config.people_site
+  self.headers["User-agent"] = "artful.ly"
+  self.element_name = 'actions'
+  self.collection_name = 'actions'
+
+  schema do
+    attribute 'person_id',  :string
+    attribute 'subject_id', :string
+    attribute 'action_type',:string
+  end
+
+  def action_type(); @attributes['action_type']; end
+  def action_type=(action_type); ;end
+
+  validates_presence_of :person_id, :subject_id
+
+  def initialize(attributes = {})
+    super(attributes)
+  end
+
+  def person
+    @person ||= find_person
+  end
+
+  def person=(person)
+    if person.nil?
+      @person = person_id = person
+      return
+    end
+
+    raise TypeError, "Expecting an AthenaPerson" unless person.kind_of? AthenaPerson
+    @person, self.person_id = person, person.id
+  end
+
+  def subject
+    @subject ||= find_subject
+  end
+
+  def subject=(subject)
+    return if subject.nil? or subject.id.nil?
+    @subject, self.subject_id = subject, subject.id
+  end
+
+  private
+    def find_person
+      return if self.person_id.nil?
+
+      begin
+        AthenaPerson.find(self.person_id)
+      rescue ActiveResource::ResourceNotFound
+        update_attribute(:person_id, nil)
+        return nil
+      end
+    end
+
+    def find_subject
+      begin
+        AthenaOrder.find(self.subject_id)
+      rescue ActiveResource::ResourceNotFound
+        update_attribute(:subject_id, nil)
+        return nil
+      end
+    end
+end
