cf285e07e8aab254d33187b4922b2b355eb737ba
Micah Frost <micah.frost@fracturedatlas.org>
2011-04-14 14:51:01 -0400

    View touch-ups for performances and orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,13 +6,8 @@ class OrdersController < ApplicationController
 
   def index
     if params[:search]
-      results = search(params[:search])
-      if results.class == AthenaOrder
-        redirect_to order_path(results.id)
-      end
-      @results = results
-    else
-      @results = "No search performed"
+      @results = search(params[:search])
+      redirect_to order_path(@results.id) if @results.is_a? AthenaOrder
     end
   end
 
@@ -34,11 +29,10 @@ class OrdersController < ApplicationController
       orders = AthenaOrder.find(query)
     rescue ActiveResource::ResourceNotFound
       ##TODO: Implement search by first name, last name, email, last four of CC number
-      orders = nil
+      []
     rescue ActiveResource::ForbiddenAccess #occurs when search string == ""
-      orders = nil
+      []
     end
-    orders
   end
 
 end
