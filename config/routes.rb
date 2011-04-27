b6cb73af9dd565ac793ca191ee8b63a3498c8b7e
Ian Guffy <Ian.Guffy@FracturedAtlas.org>
2011-04-27 16:02:44 -0400

    Splash page workflow works (still needs polish)

--- config/routes.rb
+++ config/routes.rb
@@ -75,5 +75,9 @@ Artfully::Application.routes.draw do
   match '/performances/:id/createtickets/' => 'performances#createtickets', :as => :create_tickets_for_performance
   match '/people/:id/star/:type/:action_id' => 'people#star', :as => :star, :via => "post"
 
-  root :to => "index#index"
+  match '/dashboard' => 'index#dashboard', :as => :dashboard 
+  root :to => 'index#dashboard', :constraints => lambda {|r| r.env["warden"].authenticate? }
+  root :to => 'index#index'
+  get "/" => 'index#dashboard' #, :as => "user_root"
+
 end
