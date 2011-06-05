97657c0173a75791fb65013255864ed2bd15a1b3
Ian Guffy <Ian.Guffy@FracturedAtlas.org>
2011-06-05 13:56:34 -0400

    Updated information displayed in door list

--- app/models/door_list.rb
+++ app/models/door_list.rb
@@ -9,6 +9,7 @@ class DoorList
     @items ||= performance.tickets.select(&:committed?).collect do |ticket|
       Item.new(ticket, ticket.buyer)
     end
+    @items.sort!{ |a,b| a.ticket.buyer.last_name.downcase <=> b.ticket.buyer.last_name.downcase }
   end
 
   private
