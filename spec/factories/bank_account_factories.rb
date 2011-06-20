fa0351077af9c21f25eece960145ecae794ea23d
Micah Frost <micah.frost@fracturedatlas.org>
2011-06-20 14:40:08 -0400

    Added validations to bank account according to First ACH spec

--- spec/factories/bank_account_factories.rb
+++ spec/factories/bank_account_factories.rb
@@ -1,11 +1,11 @@
 Factory.define(:bank_account) do |ba|
   ba.routing_number "111111118"
-  ba.number         "3215240125"
+  ba.number         "32152401253215240125"
   ba.account_type   "Personal Checking"
   ba.name           "Joe Smith"
   ba.address        "248 W 35th St"
   ba.city           "New York"
   ba.state          "NY"
   ba.zip            "12345"
-  ba.phone          "123-789- 4568"
+  ba.phone          "123-789-4568"
 end
