5d275d5b53d8f374f39e09ae26428ad9246ed51e
Gary Moore <gary.moore@fracturedatlas.org>
2011-06-30 14:51:40 -0400

    Sorting settlements

--- app/controllers/settlements_controller.rb
+++ app/controllers/settlements_controller.rb
@@ -1,6 +1,8 @@
 class SettlementsController < ApplicationController
   def index
-    @settlements = Settlement.find_by_organization_id(current_user.current_organization.id).paginate(:page => params[:page], :per_page => 10)
+    @settlements = Settlement.find_by_organization_id(current_user.current_organization.id)
+    @settlements = @settlements.sort{|a,b| b.created_at <=> a.created_at }
+    @settlements = @settlements.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
