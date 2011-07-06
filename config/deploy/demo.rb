40eb2db3248ccb0e0cee100e1295b245a7bd39f0
Gary Moore <gary.moore@fracturedatlas.org>
2011-07-06 11:41:28 -0400

    Symlinking db on demo

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -9,8 +9,8 @@ namespace :deploy do
   task :start do ; end
   task :stop do ; end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "#{try_sudo} ln -s #{File.join(deploy_to, 'shared', 'config', 'database.yml')} #{File.join(current_path,'config', 'database.yml')}"
     run "#{try_sudo} ln -s #{File.join(deploy_to, 'shared', 'db', 'demo.sqlite3')} #{File.join(current_path,'db', 'demo.sqlite3')}"
-   #run "#{try_sudo} cp #{File.join(deploy_to, 'shared', 'config', 'environments', '*')} #{File.join(current_path,'config', 'environments')}"
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
