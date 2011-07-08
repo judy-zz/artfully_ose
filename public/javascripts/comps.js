30bdc6f66b2bd188fac5011d2245fd999bbf242a
Micah Frost <micah.frost@fracturedatlas.org>
2011-07-08 09:05:33 -0400

    Disable inline person creation submit form while processing

--- public/javascripts/comps.js
+++ public/javascripts/comps.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
   $("#new_athena_person").bind("ajax:success", function(xhr, person){
-    console.log(person)
     var $target = $(".hidden-target").clone();
     $target.find("input[name='person_id']").val(person.id);
     $target.find(".name").html(person.first_name + " " + person.last_name + " (" + person.email + ")");
     $li = $(document.createElement('li'));
     $li.append($target.children()).appendTo(".wizard-list");
+    $(this).find("input:submit").removeAttr('disabled');
   });
 
   $("#new_athena_person").bind("ajax:error", function(xhr, status, error){
+    $(this).find("input:submit").removeAttr('disabled');
     data = eval("(" + status.responseText + ")");
     for(var i = 0; i < data.errors.length; i++){
       $.gritter.add({
@@ -16,5 +17,9 @@ $(document).ready(function(){
         text: data.errors[i]
       });
     }
+  });
+
+  $("#new_athena_person").live("ajax:before", function(){
+    $(this).find("input:submit").attr('disabled','disabled');
   });
 });
