5c37ba44aa2645e9743ae1c98af6705c0ee90a9e
Micah Frost <micah.frost@fracturedatlas.org>
2011-07-11 15:15:53 -0400

    Turn off registration, fix admin invitations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :suspendable, :invitable
 
