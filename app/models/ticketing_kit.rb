645e20e10965e042fa1a11dcadd72b5a8522588a
Micah Frost <micah.frost@fracturedatlas.org>
2011-07-12 11:53:02 -0400

    Ticketing kit requires bank account on the organization

--- app/models/ticketing_kit.rb
+++ app/models/ticketing_kit.rb
@@ -1,6 +1,7 @@
 class TicketingKit < Kit
   acts_as_kit :with_approval => true do
     activate_kit :unless => :no_owner?
+    activate_kit :unless => :no_bank_account?
 
     when_active do |organization|
       organization.can :access, :paid_ticketing
@@ -15,6 +16,11 @@ class TicketingKit < Kit
   def no_owner?
     errors.add(:requirements, "You need to be part of an organization to activate this kit.") if organization.owner.nil?
     organization.owner.nil?
+  end
+
+  def no_bank_account?
+    errors.add(:requirements, "Your organization needs bank account information first.") if organization.bank_account.nil?
+    organization.bank_account.nil?
   end
 
 end
