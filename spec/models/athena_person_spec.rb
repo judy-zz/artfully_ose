0c0e08c889a6e4120769ff6e6580c1454d8bf7d6
Micah Frost <micah.frost@fracturedatlas.org>
2011-07-18 15:49:38 -0400

    Validate uniquess of person record if email address is present

--- spec/models/athena_person_spec.rb
+++ spec/models/athena_person_spec.rb
@@ -3,22 +3,24 @@ require 'spec_helper'
 describe AthenaPerson do
   subject { Factory(:athena_person) }
 
+  before(:each) do
+    FakeWeb.register_uri(:get, %r|http://localhost/athena/people\.json\?email=.*&organizationId=.*|, :body => "[]")
+  end
+
   it { should be_valid }
   it { should respond_to :email }
 
-  it "should be valid without an email address" do
+  it "is not valid without an email address" do
     subject.email = nil
-    subject.should be_valid
+    subject.should_not be_valid
   end
 
-  it "should be valid with only a last name" do
-    subject.email = nil
+  it "is valid without a first_name" do
     subject.first_name = nil
     subject.should be_valid
   end
 
-  it "should be valid with only an email address" do
-    subject.first_name = nil
+  it "is valid without a last_name" do
     subject.last_name = nil
     subject.should be_valid
   end
@@ -57,6 +59,14 @@ describe AthenaPerson do
   describe "organization" do
     it { should respond_to :organization }
     it { should respond_to :organization_id }
+  end
+
+  describe "uniqueness" do
+    subject { Factory(:athena_person) }
+    it "should not be valid if another person record exists with that email for the organization" do
+      Factory(:athena_person_with_id, :email => subject.email, :organization => subject.organization)
+      subject.should_not be_valid
+    end
   end
 
 end
