b323d146a119ae1c369f3a5cea3ea34ba7474bbb
Micah Frost <micah.frost@fracturedatlas.org>
2011-07-18 13:27:12 -0400

    Removed extraneous lines from athena lock spec

--- spec/models/athena_lock_spec.rb
+++ spec/models/athena_lock_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe AthenaLock do
-  before(:each) do
-    AthenaLock.site = 'http://localhost/'
-  end
-
   describe "attributes" do
     subject { Factory(:lock) }
 
