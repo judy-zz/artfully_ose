90405e2f269dbf2e0f18939227a0ac5745cc7fd0
Micah Frost <micah.frost@fracturedatlas.org>
2011-07-28 17:07:29 -0400

    Cleaned up donation spec

--- spec/models/donation_spec.rb
+++ spec/models/donation_spec.rb
@@ -9,17 +9,17 @@ describe Donation do
   it { should respond_to :order }
 
   describe ".amount" do
-    it "should not be valid without an amount" do
+    it "is not be valid without an amount" do
       subject.amount = nil
       subject.should_not be_valid
     end
 
-    it "should not be valid with an amount less than 0" do
+    it "is not be valid with an amount less than 0" do
       subject.amount = -1
       subject.should_not be_valid
     end
 
-    it "should not be valid with an amount equal to 0" do
+    it "is not be valid with an amount equal to 0" do
       subject.amount = 0
       subject.should_not be_valid
     end
