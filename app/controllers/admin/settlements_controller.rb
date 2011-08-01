c2bd9276ff4d25cf9a9f7891b37f4e836d66b57f
Ian Guffy <Ian.Guffy@FracturedAtlas.org>
2011-08-01 19:19:13 -0400

    Settlements now searchable like orders. Complete but not tested with any settlements.

--- app/controllers/admin/settlements_controller.rb
+++ app/controllers/admin/settlements_controller.rb
@@ -1,6 +1,20 @@
 class Admin::SettlementsController < Admin::AdminController
   def index
-    @settlements = Settlement.find(:all, :params =>{ :createdAt => "lt#{DateTime.now.xmlschema}"}).paginate(:page => params[:page], :per_page => 25)
+    begin
+      Time.zone = current_user.current_organization.time_zone
+      unless params[:commit].blank?
+        @start = Time.zone.parse(Date.strptime(params[:start], "%m/%d/%Y").to_s)
+        @stop  = Time.zone.parse(Date.strptime(params[:stop] , "%m/%d/%Y").to_s).end_of_day
+      else
+        @start = DateTime.now.in_time_zone(Time.zone).beginning_of_month
+        @stop  = DateTime.now.in_time_zone(Time.zone).end_of_day
+      end
+      settlements_range = Settlement.in_range(@start, @stop, current_user.current_organization.id)
+      @settlements_range = settlements_range.sort{|a,b| b.timestamp <=> a.timestamp }.paginate(:page => params[:page], :per_page => 25)
+    rescue ArgumentError
+      flash[:alert] = "One or both of the dates entered are invalid."
+      redirect_to :back
+    end
   end
 
   def new
