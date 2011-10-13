e3d16525033418626f395e09496a7e33ce1d36e6
Micah Frost <micah.frost@fracturedatlas.org>
2011-10-13 10:00:19 -0400

    Organized kits into subdir

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ module Artfully
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += Dir["#{config.root}/lib/**/"] # include all subdirectories
-    config.autoload_paths += %W( #{::Rails.root.to_s}/app/models/actions )
+    config.autoload_paths += %W[
+      #{::Rails.root.to_s}/app/models/actions
+      #{::Rails.root.to_s}/app/models/kits
+    ]
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
