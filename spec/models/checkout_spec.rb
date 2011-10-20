40491b498f9b399af8743813534da43a5c9c5e2d
Micah Frost <micah.frost@fracturedatlas.org>
2011-10-20 09:58:15 -0400

    Fixed broken checkout spec

--- spec/models/checkout_spec.rb
+++ spec/models/checkout_spec.rb
@@ -16,31 +16,31 @@ describe Checkout do
     subject.should_not be_valid
   end
 
-  it "should be valid without a payment if the order total is 0 (Free)" do
+  it "should be valid without a payment if the cart total is 0 (Free)" do
     subject.payment.credit_card = nil
     subject.should be_valid
   end
 
-  it "should not be valid without an order" do
-    subject.order = nil
+  it "should not be valid without an cart" do
+    subject.cart = nil
     subject.should_not be_valid
   end
 
-  it "should not be valid if the payment is invalid and order total > 0 (Not Free)" do
+  it "should not be valid if the payment is invalid and cart total > 0 (Not Free)" do
     subject = Checkout.new(Factory(:cart_with_items), payment)
     subject.payment.stub(:valid?).and_return(false)
     subject.should_not be_valid
   end
 
-  it "should not be valid if the payment is invalid but the order total is 0 (Free)" do
+  it "should not be valid if the payment is invalid but the cart total is 0 (Free)" do
     subject.payment.stub(:valid?).and_return(false)
     subject.should_not be_valid
   end
 
   describe "cash payments" do
     let(:payment)         { CashPayment.new(Factory(:person)) }
-    let(:order_with_item) { Factory(:cart_with_items) }
-    subject               { Checkout.new(order_with_item, payment) }
+    let(:cart_with_item)  { Factory(:cart_with_items) }
+    subject               { Checkout.new(cart_with_item, payment) }
 
     it "should always approve orders with cash payments" do
       subject.stub(:create_order).and_return(Order.new)
@@ -51,7 +51,7 @@ describe Checkout do
 
   describe "#finish" do
     before(:each) do
-      subject.order.stub(:pay_with)
+      subject.cart.stub(:pay_with)
     end
 
     describe "return value" do
@@ -60,12 +60,12 @@ describe Checkout do
       end
 
       it "returns true if the order was approved" do
-        subject.order.stub(:approved?).and_return(true)
+        subject.cart.stub(:approved?).and_return(true)
         subject.finish.should be_true
       end
 
       it "returns false if the order is not approved" do
-        subject.order.stub(:approved?).and_return(false)
+        subject.cart.stub(:approved?).and_return(false)
         subject.finish.should be_false
       end
     end
@@ -86,14 +86,14 @@ describe Checkout do
       }
 
       it "should create a person record when finishing with a new customer" do
-        subject.order.stub(:organizations_from_tickets).and_return(Array.wrap(organization))
+        subject.cart.stub(:organizations_from_tickets).and_return(Array.wrap(organization))
         Person.should_receive(:find_by_email_and_organization).with(email, organization).and_return(nil)
         Person.should_receive(:create).with(attributes).and_return(Factory(:person,attributes))
         subject.finish
       end
 
       it "should not create a person record when the person already exists" do
-        subject.order.stub(:organizations_from_tickets).and_return(Array.wrap(organization))
+        subject.cart.stub(:organizations_from_tickets).and_return(Array.wrap(organization))
         Person.should_receive(:find_by_email_and_organization).with(email, organization).and_return(Factory(:person,attributes))
         Person.should_not_receive(:create)
         subject.finish
