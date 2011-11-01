ac913d1fd25fcad82e80fe9a5a510e03abff544c
Alex McHale <alex@anticlever.com>
2011-11-01 10:01:12 -0500

    Fix a typo in the export file names.

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -3,13 +3,13 @@ class ExportController < ApplicationController
   def contacts
     @organization = current_user.current_organization
     @people = AthenaPerson.find_by_organization(@organization)
-    @filename = "Artfuly-People-Export-#{DateTime.now.strftime("%m-%d-%y")}.csv"
+    @filename = "Artfully-People-Export-#{DateTime.now.strftime("%m-%d-%y")}.csv"
     render :csv => @people, :filename => @filename
   end
 
   def donations
     @organization = current_user.current_organization
-    @filename = "Artfuly-Donations-Export-#{DateTime.now.strftime("%m-%d-%y")}.csv"
+    @filename = "Artfully-Donations-Export-#{DateTime.now.strftime("%m-%d-%y")}.csv"
     @csv_string = FasterCSV.generate do |csv|
       csv << [ "Donation Date", "Gift Amount", "Non-gift Amount" ]
       @organization.all_donations do |order, item|
@@ -21,7 +21,7 @@ class ExportController < ApplicationController
 
   def ticket_sales
     @organization = current_user.current_organization
-    @filename = "Artfuly-Donations-Export-#{DateTime.now.strftime("%m-%d-%y")}.csv"
+    @filename = "Artfully-Donations-Export-#{DateTime.now.strftime("%m-%d-%y")}.csv"
     @csv_string = FasterCSV.generate do |csv|
       csv << [ "Date of Purchase", "Price" ]
       @organization.all_ticket_sales do |order, item|
