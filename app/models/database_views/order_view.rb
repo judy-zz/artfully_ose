886d207837bee2af779798860d84276390ddb4ce
Gary Moore <gary.moore@fracturedatlas.org>
2012-01-12 16:50:54 -0500

    Added some default scoping, artfully scope, and added more fields to the view

--- app/models/database_views/order_view.rb
+++ app/models/database_views/order_view.rb
@@ -2,6 +2,13 @@ class OrderView < ActiveRecord::Base
   set_table_name 'order_view'
   has_many :items, :foreign_key => 'order_id'
   
+  default_scope :order => 'created_at DESC'
+  scope :before, lambda { |time| where("created_at < ?", time) }
+  scope :after,  lambda { |time| where("created_at > ?", time) }
+  scope :imported, where("fa_id IS NOT NULL")
+  scope :not_imported, where("fa_id IS NULL")
+  scope :artfully, where("transaction_id IS NOT NULL")
+  
   def total
     items.inject(0) {|sum, item| sum + item.price.to_i }
   end
--- db/migrate/20120110224141_create_order_view.rb
+++ db/migrate/20120110224141_create_order_view.rb
@@ -1,7 +1,7 @@
 class CreateOrderView < ActiveRecord::Migration
   def self.up
     execute "CREATE OR REPLACE VIEW order_view AS " + 
-            "SELECT o.id, o.transaction_id, o.price, o.service_fee, o.organization_id, " + 
+            "SELECT o.id, o.created_at, o.updated_at, o.transaction_id, o.price, o.service_fee, o.fa_id, o.organization_id, " + 
             "o.person_id, orgs.name as organization_name, p.first_name as person_first_name, " + 
             "p.last_name as person_last_name FROM orders o " + 
             "LEFT JOIN people p " + 
