374cd1bcf44cfe330584accb749ce7d4de6431e0
Gary Moore <gary.moore@fracturedatlas.org>
2012-01-26 14:34:07 -0500

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -220,6 +220,21 @@ ActiveRecord::Schema.define(:version => 20120125171623) do
     t.integer "organization_id"
   end
 
+  create_table "order_view", :id => false, :force => true do |t|
+    t.integer  "id",                :default => 0, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "transaction_id"
+    t.integer  "price"
+    t.integer  "service_fee"
+    t.integer  "fa_id"
+    t.integer  "organization_id"
+    t.integer  "person_id"
+    t.string   "organization_name"
+    t.string   "person_first_name"
+    t.string   "person_last_name"
+  end
+
   create_table "orders", :force => true do |t|
     t.string   "transaction_id"
     t.integer  "price"
