dc3ebc501cefef8123bf56e8ad7734cd9d393c62
Alex McHale <alex@anticlever.com>
2012-02-15 13:12:46 -0600

    Validate reseller profile URL and Fee.

--- app/models/reseller_profile.rb
+++ app/models/reseller_profile.rb
@@ -1,13 +1,13 @@
 class ResellerProfile < ActiveRecord::Base
 
-  FEE_OPTIONS = { "$0.00" => 0, "$0.50" => 50, "$1.00" => 100, "$2.00" => 200 }
-
   belongs_to :organization
   has_many :ticket_offers
   has_many :reseller_events
   has_many :reseller_shows
   has_many :reseller_attachments
 
+  validates_format_of :url, :with => URI::regexp(%w( http https ))
   validates_length_of :description, :maximum => 350
+  validates_presence_of :fee
 
 end
