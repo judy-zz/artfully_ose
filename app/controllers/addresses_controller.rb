0b6a2183b66193c8a7f97d259df4fc8252f688c5
Gary Moore <gary.moore@fracturedatlas.org>
2012-02-22 14:13:55 -0500

    Adding a note when addres is updated

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -16,6 +16,11 @@ class AddressesController < ApplicationController
     o_addr = Marshal.load(Marshal.dump(@person.address))
     n_addr = params[:address]
     if @person.address.update_attributes(params[:address])
+      for f in [:address1, :address2, :city, :state, :zip, :country]
+        if o_addr[f] != n_addr[f]
+          @person.notes.create({ :occurred_at => now, :text => "#{f} updated, old #{f} was: (#{o_addr[f]})" })
+        end
+      end
       flash[:notice] = "Successfully updated the address for #{@person.first_name} #{@person.last_name}."
     else
       flash[:error] = "There was a problem updating this address."
