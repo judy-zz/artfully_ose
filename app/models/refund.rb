6633f58caa34470a9dd6c5b26f202287ad8d4df5
Gary Moore <gary.moore@gmail.com>
2012-02-28 12:13:29 -0500

    Switching to direct Braintree via activemerchant for refunds

--- app/models/refund.rb
+++ app/models/refund.rb
@@ -8,15 +8,23 @@ class Refund
 
   def submit(options = {})
     should_return = options[:and_return] || false
+    
+    #ActiveMerchant::Billing::Base.mode = :test
 
-    payment.refund!
+    gateway = ActiveMerchant::Billing::BraintreeGateway.new(
+      :merchant_id => Artfully::Application.config.BRAINTREE_MERCHANT_ID,
+      :public_key  => Artfully::Application.config.BRAINTREE_PUBLIC_KEY,
+      :private_key => Artfully::Application.config.BRAINTREE_PRIVATE_KEY
+    )
 
-    if @success = payment.refunded?
+    response = gateway.refund(refund_amount, order.transaction_id)
+    @success = response.success?
+    if @success
       items.each(&:return!) if should_return
       items.each(&:refund!)
       create_refund_order
     else
-      @gateway_error_message = payment.message
+      @gateway_error_message = response.message
     end
   end
 
--- spec/models/refund_spec.rb
+++ spec/models/refund_spec.rb
@@ -5,24 +5,31 @@ describe Refund do
   let(:order) { Factory(:order) }
   let(:items) { 3.times.collect { Factory(:item, :order => order)}}
   subject { Refund.new(order, items) }
+  
+  gateway = ActiveMerchant::Billing::BraintreeGateway.new(
+      :merchant_id => Artfully::Application.config.BRAINTREE_MERCHANT_ID,
+      :public_key  => Artfully::Application.config.BRAINTREE_PUBLIC_KEY,
+      :private_key => Artfully::Application.config.BRAINTREE_PRIVATE_KEY
+    )    
+  
+  successful_response = ActiveMerchant::Billing::Response.new(true, 'nice job!')
+  fail_response = ActiveMerchant::Billing::Response.new(false, 'you failed!')
+  
+  before(:each) do
+    ActiveMerchant::Billing::BraintreeGateway.stub(:new).and_return(gateway)
+  end
 
   describe "#submit" do
     before(:each) do
-      FakeWeb.register_uri(:post, "http://localhost/payments/transactions/refund", :body => "{ success: true }")
+      gateway.should_receive(:refund).with(3000, order.transaction_id).and_return(successful_response)
+      
       subject.items.each { |i| i.stub(:return!) }
       subject.items.each { |i| i.stub(:refund!) }
     end
 
     it "should attempt to refund the payment made for the order" do
       subject.submit
-      FakeWeb.last_request.method.should eq "POST"
-      FakeWeb.last_request.path.should eq "/payments/transactions/refund"
-    end
-
-    it "should include the total price of all items being refunded" do
-      total = subject.refund_amount / 100.0
-      subject.submit
-      FakeWeb.last_request.body.should match Regexp.new(/\"amount\":#{total}/)
+      subject.should be_successful
     end
     
     it "should create a refund_order with refunded items" do
@@ -44,41 +51,40 @@ describe Refund do
       subject.refund_order.parent.should eq order   
     end
   end
-
+  
   describe "refund_amount" do
     it "should return the total for the items in the refund in cents" do
       total = items.collect(&:price).reduce(:+)
       subject.refund_amount.should eq total
     end
   end
-
+  
   describe "successful?" do
     before(:each) do
       subject.items.each { |i| i.stub(:return!) }
       subject.items.each { |i| i.stub(:refund!) }
       subject.stub(:create_refund_order)
     end
-
+  
     it "should return false if it has not been submitted" do
       subject.should_not be_successful
     end
-
+  
     it "should return true if the refund was successful" do
-      FakeWeb.register_uri(:post, 'http://localhost/payments/transactions/refund', :body => '{ "success": true }')
+      gateway.should_receive(:refund).with(3000, order.transaction_id).and_return(successful_response)
       subject.submit
       subject.should be_successful
     end
-
+  
     it "should return false if the refund was not successful" do
-      FakeWeb.register_uri(:post, 'http://localhost/payments/transactions/refund', :body => '{ "success": false }')
+      gateway.should_receive(:refund).with(3000, order.transaction_id).and_return(fail_response)
       subject.submit
       subject.should_not be_successful
     end
   end
-
+  
   describe "a partial refund" do
     before(:each) do
-      FakeWeb.register_uri(:post, "http://localhost/payments/transactions/refund", :body => "{ success: true }")
       subject.items.each { |i| i.stub(:return!) }
       subject.items.each { |i| i.stub(:refund!) }
       subject.stub(:create_refund_order)
@@ -92,9 +98,9 @@ describe Refund do
     
     it "should issue a refund for the amount being refunded" do
       refundable_items = items[0..1]
+      gateway.should_receive(:refund).with(2000, order.transaction_id).and_return(successful_response)
       partial_refund = Refund.new(order, refundable_items)
       partial_refund.submit
-      (FakeWeb.last_request.body.include? "20.0").should be_true
       partial_refund.items.length.should eq 2
     end
   end
