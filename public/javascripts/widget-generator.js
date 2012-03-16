972e8a0a244a60dde3b84e2e5ee0eba8c8cd52e4
Gary Moore <gary.moore@gmail.com>
2012-03-16 16:40:30 -0400

    Lots of work on widget

--- public/javascripts/widget-generator.js
+++ public/javascripts/widget-generator.js
@@ -1,17 +1,30 @@
+var eventEnabled = true
+
 $("document").ready(function(){
-	
-	$('#generate-link').bind("click", function() {
-		$('.the-code').removeClass('hidden')
-		return false;
-	})
 
   $(".widget_type").change(function(){
 		console.log($(this).attr('value'))
     if($(this).attr('value') == 'event' || $(this).attr('value') == 'both') { 
       $('.events').removeClass('hidden');
+			eventEnabled = true
     } else {
       $('.events').addClass('hidden');
+			eventEnabled = false
     }   
   });
 
 })
+
+$("#widget-form").bind("ajax:beforeSend", function(evt, data, status, xhr){
+	if (eventEnabled && $('#event_id').val() == "") {
+		setErrorMessage("Please select an event")
+		return false;
+	} else {
+		$('.flash').remove()
+	}
+});
+
+$("#widget-form").bind("ajax:success", function(evt, data, status, xhr){
+	$('.the-code').removeClass('hidden')
+	$('#widget-code').html(data)
+});
