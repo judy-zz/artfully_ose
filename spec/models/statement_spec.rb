3147e7f4e60bfb512d8b7b417694f6ce8b625863
Gary Moore <gary.moore@fracturedatlas.org>
2012-03-21 15:26:11 -0400

    Statement spec fix in progress

--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -2,14 +2,12 @@ require 'spec_helper'
 
 describe Statement do
   disconnect_sunspot  
-  let(:organization) { Factory(:organization) }
-  let(:show) { Factory(:show) }
-  let(:tickets) { 10.times.collect { Factory(:ticket, :show => show, :section => Factory(:section)) } }
-  let(:payment) { Factory(:payment) }
-  let(:cart) { Factory(:cart_with_only_tickets, :tickets => tickets[6..8]) }
-
-  let(:checkout) { Checkout.new(cart, payment) }  
-  let(:statement) { Statement.for_show(show, organization) }
+  let(:organization)    { Factory(:organization) }
+  let(:event)           { Factory(:event) }
+  let(:paid_chart)      { Factory(:assigned_chart, :event => event) }
+  let(:paid_show)       { Factory(:show, :organization => Factory(:organization), :chart => paid_chart, :event => event) }
+  let(:payment)         { Factory(:payment) }
+  let(:cart)            { Factory(:cart_with_only_tickets, :tickets => show.tickets[6..8]) }
   
   describe "nil show" do
     it "should return an empty statement if the show is nil" do
@@ -19,163 +17,161 @@ describe Statement do
     end
   end
   
-  describe "free show" do
-    before(:each) do
-      tickets.each do |ticket|
-        ticket.price = 0
-        ticket.on_sale!
-      end
-      checkout.finish
-    end
-    
-    it "should report the date of the show" do
-      statement.datetime.should eq show.datetime_local_to_event
-    end
+  # describe "free show" do
+  #   before(:each) do
+  #     tickets.each do |ticket|
+  #       ticket.price = 0
+  #       ticket.on_sale!
+  #     end
+  #     checkout.finish
+  #   end
+  #   
+  #   it "should report the date of the show" do
+  #     statement.datetime.should eq show.datetime_local_to_event
+  #   end
+  # 
+  #   it "should report how many tickets were sold" do
+  #     statement.tickets_sold.should eq 3
+  #   end
+  # 
+  #   it "should report the potential revenue of the show" do
+  #     statement.potential_revenue.should eq 0
+  #   end
+  # 
+  #   it "should report how many comps were given to the show" do
+  #     statement.tickets_comped.should eq 0
+  #   end
+  # 
+  #   it "should report the gross as num items sold times the gross price of the items" do
+  #     statement.gross_revenue.should eq 0
+  #   end  
+  #   
+  #   it "should report processing as 3.5% of the gross" do
+  #     statement.processing.should eq (0 * 0.035)
+  #   end
+  # 
+  #   it "should report the net as gross - processing" do
+  #     statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
+  #   end  
+  # end
   
-    it "should report how many tickets were sold" do
-      statement.tickets_sold.should eq 3
-    end
+  # describe "all tickets comped" do
+  #   before(:each) do
+  #     tickets.each do |ticket|
+  #       ticket.on_sale!
+  #       ticket.comp!
+  #       ticket.save
+  #     end
+  #   end
+  #   
+  #   it "should report the date of the show" do
+  #     statement.datetime.should eq show.datetime
+  #   end
+  # 
+  #   it "should report how many tickets were sold" do
+  #     statement.tickets_sold.should eq 0
+  #   end
+  # 
+  #   it "should report the potential revenue of the show" do
+  #     statement.potential_revenue.should eq 50000
+  #   end
+  # 
+  #   it "should report how many comps were given to the show" do
+  #     statement.tickets_comped.should eq 10
+  #   end
+  # 
+  #   it "should report the gross as num items sold times the gross price of the items" do
+  #     statement.gross_revenue.should eq 0
+  #   end  
+  #   
+  #   it "should report processing as 3.5% of the gross" do
+  #     statement.processing.should eq (0 * 0.035)
+  #   end
+  # 
+  #   it "should report the net as gross - processing" do
+  #     statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
+  #   end  
+  # end
   
-    it "should report the potential revenue of the show" do
-      statement.potential_revenue.should eq 0
-    end
-  
-    it "should report how many comps were given to the show" do
-      statement.tickets_comped.should eq 0
-    end
-  
-    it "should report the gross as num items sold times the gross price of the items" do
-      statement.gross_revenue.should eq 0
-    end  
-    
-    it "should report processing as 3.5% of the gross" do
-      statement.processing.should eq (0 * 0.035)
-    end
-  
-    it "should report the net as gross - processing" do
-      statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
-    end  
-  end
-  
-  describe "all tickets comped" do
-    before(:each) do
-      tickets.each do |ticket|
-        ticket.on_sale!
-        ticket.comp!
-        ticket.save
-      end
-    end
-    
-    it "should report the date of the show" do
-      statement.datetime.should eq show.datetime
-    end
-  
-    it "should report how many tickets were sold" do
-      statement.tickets_sold.should eq 0
-    end
-  
-    it "should report the potential revenue of the show" do
-      statement.potential_revenue.should eq 50000
-    end
-  
-    it "should report how many comps were given to the show" do
-      statement.tickets_comped.should eq 10
-    end
-  
-    it "should report the gross as num items sold times the gross price of the items" do
-      statement.gross_revenue.should eq 0
-    end  
-    
-    it "should report processing as 3.5% of the gross" do
-      statement.processing.should eq (0 * 0.035)
-    end
-  
-    it "should report the net as gross - processing" do
-      statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
-    end  
-  end
-  
-  describe "no tickets sold" do
-    before(:each) do
-      tickets.each do |ticket|
-        ticket.save
-      end
-    end
-    
-    it "should report the date of the show" do
-      statement.datetime.should eq show.datetime
-    end
-  
-    it "should report how many tickets were sold" do
-      statement.tickets_sold.should eq 0
-    end
-  
-    it "should report the potential revenue of the show" do
-      statement.potential_revenue.should eq 50000
-    end
-  
-    it "should report how many comps were given to the show" do
-      statement.tickets_comped.should eq 0
-    end
-  
-    it "should report the gross as num items sold times the gross price of the items" do
-      statement.gross_revenue.should eq 0
-    end  
-    
-    it "should report processing as 3.5% of the gross" do
-      statement.processing.should eq (0 * 0.035)
-    end
-  
-    it "should report the net as gross - processing" do
-      statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
-    end  
-  end
+  # describe "no tickets sold" do
+  #   before(:each) do
+  #     tickets.each do |ticket|
+  #       ticket.save
+  #     end
+  #   end
+  #   
+  #   it "should report the date of the show" do
+  #     statement.datetime.should eq show.datetime
+  #   end
+  # 
+  #   it "should report how many tickets were sold" do
+  #     statement.tickets_sold.should eq 0
+  #   end
+  # 
+  #   it "should report the potential revenue of the show" do
+  #     statement.potential_revenue.should eq 50000
+  #   end
+  # 
+  #   it "should report how many comps were given to the show" do
+  #     statement.tickets_comped.should eq 0
+  #   end
+  # 
+  #   it "should report the gross as num items sold times the gross price of the items" do
+  #     statement.gross_revenue.should eq 0
+  #   end  
+  #   
+  #   it "should report processing as 3.5% of the gross" do
+  #     statement.processing.should eq (0 * 0.035)
+  #   end
+  # 
+  #   it "should report the net as gross - processing" do
+  #     statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
+  #   end  
+  # end
   
   describe "happy path" do
+      
+    let(:tickets)         { paid_show.tickets[6..8] }
+    let(:cart)            { Factory(:cart_with_only_tickets, :tickets => tickets) }
+    let(:checkout)        { Checkout.new(cart, payment) }  
+    let(:statement)       { Statement.for_show(paid_show, organization) }
+      
     before(:each) do
       FakeWeb.register_uri(:post, "http://localhost/payments/transactions/authorize", :body => "{ success:true, transaction_id:'j59qrb' }")
       FakeWeb.register_uri(:post, "http://localhost/payments/transactions/settle", :body => "{ success : true }")
-      tickets.each do |ticket|
-        ticket.on_sale!
-      end
-      comp_tickets
+      
       checkout.finish
+      paid_show.tickets[0..2].each do |t|
+        t.comp!
+      end
     end
   
     it "should report the date of the show" do
-      statement.datetime.should eq show.datetime
+      statement.datetime.should eq paid_show.datetime
     end
   
     it "should report how many tickets were sold" do
       statement.tickets_sold.should eq 3
     end
-  
+      
     it "should report the potential revenue of the show" do
-      statement.potential_revenue.should eq 50000
+      statement.potential_revenue.should eq 10000
     end
-  
+      
     it "should report how many comps were given to the show" do
       statement.tickets_comped.should eq 3
     end
-  
+      
     it "should report the gross as num items sold times the gross price of the items" do
-      statement.gross_revenue.should eq 15000
+      statement.gross_revenue.should eq 3000
     end  
     
     it "should report processing as 3.5% of the gross" do
-      statement.processing.should eq (15000 * 0.035)
+      statement.processing.should be_within(0.00001).of(3000 * 0.035)
     end
-  
+      
     it "should report the net as gross - processing" do
       statement.net_revenue.should eq (statement.gross_revenue - statement.processing)
     end
-  end
-  
-  def comp_tickets
-    3.times do |i|
-      tickets[i+2].on_sale!
-      tickets[i+2].comp!
-      tickets[i+2].save
-    end  
   end
 end
