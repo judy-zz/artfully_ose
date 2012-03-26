8c447fb4c4ed4a84af62c62b6398f2b5cf04be4b
Gary Moore <gary.moore@fracturedatlas.org>
2012-03-26 16:44:27 -0400

    Starting on dashboard and org edit page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -12,7 +12,7 @@ class IndexController < ApplicationController
   def dashboard
     if current_user.is_in_organization?
       @events = current_user.current_organization.events.includes(:shows, :venue).paginate(:page => params[:page], :per_page => 10)
-      @recent_actions = Action.recent(current_user.current_organization, 5)
+      @recent_actions = Action.recent(current_user.current_organization, 10)
     end
   end
 end
