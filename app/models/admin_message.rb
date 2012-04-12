24f2a4d6b016286172188b743816c52edbfa9d08
Alex McHale <alex@anticlever.com>
2012-04-12 08:49:55 -0500

    Display active admin messages on the user dashboard.

--- app/models/admin_message.rb
+++ app/models/admin_message.rb
@@ -3,6 +3,6 @@ class AdminMessage < ActiveRecord::Base
   validates_presence_of :message
   validates_presence_of :starts_on, :ends_on
 
-  scope :active, lambda { |time| where "starts_on <= :date AND :date <= ends_on", date: time.to_date }
+  scope :active, lambda { |time = Time.now| where "starts_on <= :date AND :date <= ends_on", date: time.to_date }
 
 end
