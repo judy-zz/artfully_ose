d08a9067e0f8d9886da256b8065a6c73bd72b22a
Gary Moore <gary.moore@fracturedatlas.org>
2012-04-16 16:34:55 -0400

    Forcing paperclip to use ssl

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,6 +12,7 @@ class Event < ActiveRecord::Base
     :storage => :s3,
     :path => ":attachment/:id/:style.:extension",
     :bucket => ENV["S3_BUCKET"],
+    :s3_protocol => 'https',
     :s3_credentials => {
       :access_key_id => ENV["ACCESS_KEY_ID"],
       :secret_access_key => ENV["SECRET_ACCESS_KEY"]
