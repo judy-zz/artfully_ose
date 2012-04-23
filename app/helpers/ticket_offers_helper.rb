f662c66bb1b35e46bd68f847ab0400d01bf8599e
Alex McHale <alex@anticlever.com>
2012-04-23 09:02:59 -0500

    Clean up more of the resellers pages for bootstrap.

--- app/helpers/ticket_offers_helper.rb
+++ app/helpers/ticket_offers_helper.rb
@@ -5,11 +5,11 @@ module TicketOffersHelper
 
     case offer.status
     when "offered"
-      buttons << button_to("Accept", accept_ticket_offer_path(offer), :class => "accept button", :method => :get)
-      buttons << button_to("Decline", decline_ticket_offer_path(offer), :class => "decline button", :method => :get)
+      buttons << button_to("Accept", accept_ticket_offer_path(offer), :class => "accept button btn", :method => :get)
+      buttons << button_to("Decline", decline_ticket_offer_path(offer), :class => "decline button btn", :method => :get)
     end
 
-    raw buttons.join("<br>")
+    raw buttons.join(" ")
   end
 
   def producer_offer_control(offer)
@@ -17,10 +17,10 @@ module TicketOffersHelper
 
     case offer.status
     when "creating"
-      buttons << button_to("Prepare", edit_ticket_offer_path(offer), :class => "button", :method => :get)
+      buttons << button_to("Prepare", edit_ticket_offer_path(offer), :class => "button btn", :method => :get)
     end
 
-    raw buttons.join("<br>")
+    raw buttons.join(" ")
   end
 
 end
