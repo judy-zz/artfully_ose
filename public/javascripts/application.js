10769ff403b437735de68bf7e94fd9e6b67bf7ba
Gary Moore <gary.moore@fracturedatlas.org>
2012-04-26 15:42:55 -0400

    Opening popover on page load

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -66,6 +66,8 @@ $(document).ready(function() {
 	/*********** NEW BOOTSTRAP JS ***********/
 	$(".alert").alert()
 	
+	$('.section-price-disabled').popover({trigger:'manual'})
+	                            .click(function(){ $(this).popover('toggle') });
 	$('.section-price-disabled').popover('show');
 	
 	$('.dropdown-toggle').dropdown()
