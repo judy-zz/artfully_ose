0f9cb249a1f7681cdf19b9357050b37c32ac4463
Gary Moore <gary.moore@gmail.com>
2012-05-22 10:54:59 -0400

    Updating fax number

--- app/helpers/kit_helper.rb
+++ app/helpers/kit_helper.rb
@@ -4,7 +4,7 @@ module KitHelper
   
   def kit_message(kit)
     if kit.pending? && current_user.current_organization.bank_account.nil?
-      "We haven't processed your ACH authorization form #{link_to( "(download)", "/forms/artfully_etf_sign_up.pdf")}. If you have not already done so, please submit this form and a voided check by email to support@artful.ly or by fax to (212) 277-8025, ATTN: Artful.ly.".html_safe
+      "We haven't processed your ACH authorization form #{link_to( "(download)", "/forms/artfully_etf_sign_up.pdf")}. If you have not already done so, please submit this form and a voided check by email to support@artful.ly or by fax to (815) 366-8174, ATTN: Artful.ly.".html_safe
     else
       kit.pitch
     end
