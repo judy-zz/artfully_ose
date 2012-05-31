27f8163a913f0f3e2beca9ade417c2f1a9cf32e0
Gary Moore <gary.moore@fracturedatlas.org>
2012-05-31 16:34:46 -0400

    Person link cleanup
    Changed link_to_person to show the name, and if no name then show email, else show Anonymous if dummy.
    
    Remove person_link in lieu of link_to_person

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,13 +3,16 @@ module PeopleHelper
     link_to person.first_name + ' ' + person.last_name, person_path(person)
   end
   
+  #yuck
   def link_to_person(person)
     if person.nil?
       puts "(none)"
+    elsif person.dummy?
+      link_to "Anonymous", person_path(person)
     elsif person.last_name.blank? && person.first_name.blank?
-      link_to "Unknown", person_path(person)
+      link_to person.email, person_path(person)
     else
-      link_to "#{person.last_name}, #{person.first_name}", person_path(person)
+      link_to "#{person.first_name} #{person.last_name}", person_path(person)
     end
   end
 end
