b168d5e7ce1280b0ad36c8c4e881be167feeead8
Gary Moore <gary.moore@gmail.com>
2012-06-05 11:36:04 -0400

    Broke finace report because of pagination.  Fixed it

--- app/controllers/admin/finances_controller.rb
+++ app/controllers/admin/finances_controller.rb
@@ -2,8 +2,8 @@ class Admin::FinancesController < Admin::AdminController
   def index
     @start = params[:start].present? ? DateTime.parse(params[:start]) : Time.now.beginning_of_week
     @stop  = params[:start].present? ? DateTime.parse(params[:stop]).end_of_day : Time.now.end_of_week
-    @orders = OrderView.after(@start).before(@stop)
-    @artfully_orders = OrderView.artfully.after(@start).before(@stop)
+    @orders = OrderView.includes(:items).after(@start).before(@stop)
+    @artfully_orders = OrderView.artfully.includes(:items).after(@start).before(@stop)
     settlements_in_range = Settlement.in_range(@start, @stop)
     @settlements = settlements_in_range.sort{|a,b| b.created_at <=> a.created_at }
     @summary = FinanceSummary.new(@orders, @settlements)
