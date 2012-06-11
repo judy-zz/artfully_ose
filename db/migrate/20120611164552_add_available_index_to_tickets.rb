cdd83167b3b7eada44c9578e18e13e47ba611e63
Gary Moore <gary.moore@fracturedatlas.org>
2012-06-11 14:08:07 -0400

    May as well add an index for cart_id too

--- db/migrate/20120611164552_add_available_index_to_tickets.rb
+++ db/migrate/20120611164552_add_available_index_to_tickets.rb
@@ -1,9 +1,11 @@
 class AddAvailableIndexToTickets < ActiveRecord::Migration
   def self.up
     add_index :tickets, [:section_id, :show_id, :state]
+    add_index :tickets, :cart_id
   end
 
   def self.down
     remove_index :tickets, [:section_id, :show_id, :state]
+    remove_index :tickets, :cart_id
   end
 end
