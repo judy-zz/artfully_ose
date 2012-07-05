7a4e1f50514d632f8acb34a7e320718cbb33e91e
Gary Moore <gary.moore@fracturedatlas.org>
2012-07-05 14:36:02 -0400

    Switching dmo to a more demoable mode

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -4,11 +4,16 @@ Artfully::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # false in production
+  config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  # true in production
+  config.assets.digest = false
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
