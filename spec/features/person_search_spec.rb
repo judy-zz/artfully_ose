8756c3836009d0d8f31ec5e651ebb227bb4f38a3
Gary Moore <gary.moore@gmail.com>
2012-07-10 10:25:45 -0400

    Adding requires_solr tag to tests that require a running solr instance.  They can be filtered out with --tag ~requires_solr

--- spec/features/person_search_spec.rb
+++ spec/features/person_search_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Searching for People" do
+describe "Searching for People", :requires_solr => true do
   let!(:organization) {Factory(:organization)}
   let!(:person) { Factory(:person, 
                           first_name: "ABCDEFG",
@@ -10,33 +10,39 @@ describe "Searching for People" do
   before(:all) do
     FakeWeb.allow_net_connect = true
   end
+  
   before(:each) do
     Person.reindex
   end
+  
   context "with a full first name" do
     let(:query) {"ABCDEFG"}
     it "should return the person that matches" do
       Person.search_index(query, organization).should == [person]
     end
   end
+  
   context "with a full last name" do
     let(:query) {"HIJKLMNOP"}
     it "should return the person that matches" do
       Person.search_index(query, organization).should == [person]
     end
   end
+  
   context "with a partial query that matches the first part of the name" do
     let(:query) {"ABC"}
     it "should return the person that matches" do
       Person.search_index(query, organization).should == [person]
     end
   end
+  
   context "with a partial query that matches the last part of the name" do
     let(:query) {"EFG"}
     it "should NOT return the person that matches" do
       Person.search_index(query, organization).should_not include(person)
     end
   end
+  
   context "with a partial query that matches the middle part of the first name" do
     let(:query) {"CDE"}
     it "should NOT return the person that matches" do
