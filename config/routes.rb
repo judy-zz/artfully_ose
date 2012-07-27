3f20fbc1bde9a2de8d24b50bd8ba96c1eafd5dc7
Gary Moore <gary.moore@fracturedatlas.org>
2012-07-27 13:34:57 -0400

    (OSE) Splitting widget and storefront

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,15 @@ Rails.application.routes.draw do
       get :authorization
     end
   end
+  
+  namespace :widget do
+    resource :order, :only => [:show, :create, :update, :destroy]
+    resource :checkout
+  end
 
   namespace :store do
     resources :events, :only => :show
-    resource :order, :only => [:show, :create, :update, :destroy, :storefront_sync] do
+    resource :order, :only => [:storefront_sync] do
       collection do
         post :storefront_sync
       end
