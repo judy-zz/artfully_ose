f25d9c2d249e07e09e301054cd7a5a80297af658
Gary Moore <gary.moore@gmail.com>
2012-07-31 15:11:17 -0400

    (OSE) MOVED A RAILS 2.3 STYLE PLUGIN OUT OF VENDOR/PLUGINS!!!!!1  D2s3 is now in artfully_ose

--- lib/d2s3/s3_config.rb
+++ lib/d2s3/s3_config.rb
@@ -5,10 +5,9 @@ module D2S3
     cattr_reader :access_key_id, :secret_access_key, :bucket
 
     def self.load_config
-      @@bucket            = config[RAILS_ENV].s3.bucket
-      # @@bucket            = Artfully::Application.config.s3.bucket
-      # @@access_key_id     = Artfully::Application.config.s3.access_key_id
-      # @@secret_access_key = Artfully::Application.config.s3.secret_access_key
+      @@bucket            = Artfully::Application.config.s3.bucket
+      @@access_key_id     = Artfully::Application.config.s3.access_key_id
+      @@secret_access_key = Artfully::Application.config.s3.secret_access_key
     end
   end
 end
