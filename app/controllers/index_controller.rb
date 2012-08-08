64a10cb6af728ea97410689ad4a21d0be544374d
Gary Moore <gary.moore@gmail.com>
2012-08-07 22:14:40 -0400

    (OSE) Splitting index controller.  Pulling more stuff out of the organization model:

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -14,15 +14,6 @@ class IndexController < ArtfullyOseController
     if current_user.is_in_organization?
       @events = current_user.current_organization.events.includes(:shows, :venue).order('updated_at DESC').limit(4)
       @recent_actions = Action.recent(current_user.current_organization, 10)
-      @reseller_profile = current_user.current_organization.reseller_profile
-
-      if @reseller_profile
-        @ticket_offers = @reseller_profile.ticket_offers.offered.all
-      elsif current_user.current_organization.has_kit?(:reseller)
-        profile_path = new_organization_reseller_profile_path(current_user.current_organization)
-        anchor = %[<a href="#{profile_path}">Setup your profile</a>]
-        flash.now[:notice] = "Your reseller kit has been approved! #{anchor} so that you'll show up in the resellers directory".html_safe
-      end
     end
   end
 
