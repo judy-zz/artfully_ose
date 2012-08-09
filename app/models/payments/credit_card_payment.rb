77ef19152f28061813edd6946c28cfd06104ca80
Gary Moore <gary.moore@gmail.com>
2012-08-09 14:55:01 -0400

    (OSE) Moving payments

---
+++ app/models/payments/credit_card_payment.rb
@@ -0,0 +1,73 @@
+class CreditCardPayment < ::Payment
+  payment_method [:credit_card, :credit_card_swipe, :credit_card_manual]
+  
+  #ActiveMerchant::Billing::CreditCard
+  attr_accessor :credit_card 
+  
+  def initialize(params = {})
+    self.credit_card    ||= ActiveMerchant::Billing::CreditCard.new
+    self.customer       ||= Person.new
+    build(params) unless params.blank?
+  end
+  
+  def per_item_processing_charge
+    lambda { |item| item.realized_price * 0.035 }
+  end
+  
+  #
+  # We may be able to get some milage out of a reop called active_attr: https://github.com/cgriego/active_attr
+  #
+  def build(params)
+    [:amount, :user_agreement, :transaction_id].each do |field| 
+      self.instance_variable_set("@#{field.to_s}", params[field])
+    end
+    
+    unless params[:credit_card].nil?
+      params[:credit_card].each do |key, value| 
+        self.credit_card.send("#{key}=", value)
+      end
+    end
+    
+    unless params[:customer].nil?
+      self.customer.first_name  = params[:customer][:first_name]
+      self.customer.last_name   = params[:customer][:last_name]
+      self.customer.email       = params[:customer][:email]
+      self.customer.phones      << Phone.new(:number => params[:customer][:phone]) unless params[:customer][:phone].blank?
+      
+      self.customer.address = Address.new(:address1 => params[:customer][:address][:address1],
+                                          :city     => params[:customer][:address][:city],
+                                          :state    => params[:customer][:address][:state],
+                                          :zip      => params[:customer][:address][:zip])
+    end
+  end
+  
+  def gateway
+    @gateway ||= ActiveMerchant::Billing::BraintreeGateway.new(
+        :merchant_id => Artfully::Application.config.braintree.merchant_id,
+        :public_key  => Artfully::Application.config.braintree.public_key,
+        :private_key => Artfully::Application.config.braintree.private_key
+      )
+  end
+  
+  def requires_authorization?
+    amount > 0
+  end
+  
+  #purchase submits for auth and passes a flag to merchant to settle immediately
+  def purchase(options={})
+    response = gateway.purchase(self.amount, credit_card, options)
+    self.transaction_id = response.authorization
+    response
+  end
+  
+  def authorize(options={})
+    response = gateway.authorize(self.amount, credit_card, options)
+    self.transaction_id = response.authorization
+    response
+  end
+  
+  def capture(response, options={})
+    gateway.capture(self.amount, response.authorization, options)
+  end
+  alias :settle :capture
+end
---
+++ app/models/payments/payment.rb
@@ -0,0 +1,76 @@
+class Payment 
+  include ActiveModel::Validations
+  
+  attr_accessor :amount, :user_agreement, :transaction_id
+  
+  #This is named customer as it analogizes the "customer" record on a remote payment
+  # system (Braintree, for instance).  It is just a Person object.
+  attr_accessor :customer
+  
+  validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
+
+  #
+  # Subclasses should call payment_method :type to get hooked up to the factory
+  #
+  # For example, a subclass called WonkaPayment can call payment_method :wonka
+  # Then callers can instantiate a WonkaPayment by calling Payment.create :wonka
+  #
+  # This will also define an instance method called "payment_method" on the subclass
+  # which will return a stringification of the symbol
+  #
+  @@payment_methods = HashWithIndifferentAccess.new
+  
+  def self.payment_method names
+    names = Array.wrap(names)
+    names.each do |name|
+      @@payment_methods[name] = self
+    end
+    
+    self.class_eval(<<-EOS, __FILE__, __LINE__)
+      def payment_method
+        "#{names[0].to_s.gsub('_',' ').capitalize}"
+      end
+    EOS
+  end
+
+  #
+  # Call this method to create sub-classes of Payment.  params will be passed through to the child class
+  #
+  def self.create(type, params = {})
+    c = @@payment_methods[type]
+    if c 
+      c.new(params)
+    else
+      raise "No payment method registered for [#{type}], did you call payment_method in the subclass?"
+    end
+  end
+  
+  #
+  # Subclasses that need to actually process something should override this method
+  #
+  def purchase(options = {})
+  end
+  
+  def requires_authorization?
+    false
+  end
+  
+  def reduce_amount_by(amount_in_cents)
+    self.amount= self.amount - amount_in_cents
+  end
+  
+  def per_item_processing_charge
+    lambda { |item| item.realized_price * 0.035 }
+  end
+  
+  #
+  # This is quite a hack and should only be needed for development
+  # The Payment subclasses need to be loaded so that their payment_method stuff can run and be injected
+  # before the interpreter gets around to loading them
+  #
+  # Also, this unfortuantely means that subclasses of this method must be in this directory
+  #
+  if Rails.env.development?
+    Dir["#{Rails.root}/app/models/payments/*.rb"].each { |model| require_dependency model }
+  end
+end
