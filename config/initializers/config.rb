86a73aa690794755925d1cc6c57b61bf71a5c51c
Gary Moore <gary.moore@fracturedatlas.org>
2012-08-13 15:54:57 -0400

    (OSE) Change yaml loading to has with indiff access

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,3 +1,3 @@
 require 'yaml'
 
-ARTFULLY_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/artfully.yml")[Rails.env]
+ARTFULLY_CONFIG = HashWithIndifferentAccess.new(YAML.load_file("#{Rails.root.to_s}/config/artfully.yml")[Rails.env])
