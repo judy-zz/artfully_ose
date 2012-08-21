e6e40bbc94abe475b20f6569bb1cc4055579f3c7
Clinton Judy <clinton@j-udy.com>
2012-08-21 14:14:49 -0400

    Allow more views to use shorthand for displaying people.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,20 +3,7 @@ module PeopleHelper
     link_to person.first_name + ' ' + person.last_name, person_path(person)
   end
   
-  def display_name(person)
-    if person.nil?
-      "(none)"
-    elsif person.dummy?
-      "Anonymous"
-    elsif person.last_name.blank? && person.first_name.blank?
-      person.email
-    else
-      "#{person.first_name} #{person.last_name}"
-    end
-  end
-  
-  #yuck
   def link_to_person(person)
-    link_to display_name(person), person_path(person)
+    link_to person, person
   end
 end
