712676bb88de80786c69f0a1720ca25f4e7c96d3
Gary Moore <gary.moore@gmail.com>
2012-08-23 14:03:58 -0400

    Convert search params to AdminTimeZone.time_zone before searching for orders.  Needed because Our timestamp (UTC) and Braintree (EST) were making searches incorrect

--- app/controllers/admin/finances_controller.rb
+++ app/controllers/admin/finances_controller.rb
@@ -1,11 +1,23 @@
 class Admin::FinancesController < Admin::AdminController
-  def index
-    @start = params[:start].present? ? DateTime.parse(params[:start]) : Time.now.beginning_of_week
-    @stop  = params[:start].present? ? DateTime.parse(params[:stop]).end_of_day : Time.now.end_of_week
-    @orders = OrderView.includes(:items).after(@start).before(@stop)
-    @artfully_orders = OrderView.artfully.includes(:items).after(@start).before(@stop)
-    settlements_in_range = Settlement.in_range(@start, @stop)
+  include AdminTimeZone
+  
+  def index    
+    @orders = OrderView.includes(:items).after(start).before(stop)
+    @artfully_orders = OrderView.artfully.includes(:items).after(start).before(stop)
+    settlements_in_range = Settlement.in_range(start, stop)
     @settlements = settlements_in_range.sort{|a,b| b.created_at <=> a.created_at }
     @summary = FinanceSummary.new(@orders, @settlements)
+  end
+  
+  def start
+    @start ||= params[:start].present? ?
+               ActiveSupport::TimeZone.create(time_zone).parse(params[:start]) :
+               Time.now.beginning_of_week
+  end
+  
+  def stop    
+    @stop  ||= params[:stop].present? ? 
+               ActiveSupport::TimeZone.create(time_zone).parse(params[:stop]).end_of_day : 
+               Time.now.end_of_week
   end
 end
