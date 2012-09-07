8d77eb841b4e455958f20ab4155dcd7012f19702
Clinton Judy <clinton@j-udy.com>
2012-09-07 16:45:49 -0400

    (OSE) Whoops, use 1.8.7 syntax.

--- spec/factories/payments_factories.rb
+++ spec/factories/payments_factories.rb
@@ -1,6 +1,6 @@
 require 'ostruct'
 FactoryGirl.define do
-  factory :customer, class: OpenStruct do
+  factory :customer, :class => OpenStruct do
     first_name = Faker::Name.first_name
     last_name   = Faker::Name.last_name
     phone       = Faker::PhoneNumber.phone_number
