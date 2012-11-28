f2c689e383bed2695c6f73c1b938d6bf4472434c
Clinton Judy <clinton@j-udy.com>
2012-11-28 14:26:35 -0500

    (OSE) Fix a few statement specs.
    They all need credit card order factory.

--- spec/factories/order_factories.rb
+++ spec/factories/order_factories.rb
@@ -2,8 +2,17 @@ FactoryGirl.define do
   factory :order do
     transaction_id "j59qrb"
     price 50
-    association :person
-    association :organization
+    person
+    organization
+  end
+  
+  factory :credit_card_order, :parent => :order do
+    transaction_id "j59qrb"
+    price 50
+    person
+    organization
+    payment_method ::CreditCardPayment.payment_method
+    per_item_processing_charge { lambda { |item| item.realized_price * 0.035 } }
   end
   
   factory :order_with_processing_charge, :parent => :order do
