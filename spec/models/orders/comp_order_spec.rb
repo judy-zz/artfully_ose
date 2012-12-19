481d59e03c5e0cfd16e47fcb27efd53b6b8e9860
Gary Moore <gary.moore@fracturedatlas.org>
2012-12-19 10:26:56 -0500

    (OSE) More spcing around refunds

--- spec/models/orders/comp_order_spec.rb
+++ spec/models/orders/comp_order_spec.rb
@@ -4,10 +4,8 @@ describe CompOrder do
   disconnect_sunspot
   subject { FactoryGirl.build(:comp_order) }
 
-  describe "refundable_items" do
-    it "shouldn't allow refunds" do
-      subject.items << FactoryGirl.create(:comped_item)
-      subject.refundable_items.length.should eq 0
-    end
+  it "shouldn't return any refundable items" do
+    subject.items << FactoryGirl.create(:comped_item)
+    subject.refundable_items.length.should eq 0
   end
 end
