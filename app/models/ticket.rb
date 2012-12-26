975cec56579b7ec4b01b5014f2931add20722835
Gary Moore <gary.moore@gmail.com>
2012-12-26 15:37:46 -0500

    (OSE) Remove form cart when exchanging

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -202,6 +202,7 @@ class Ticket < ActiveRecord::Base
   end
 
   def return!(and_return_to_inventory = true)
+    remove_from_cart
     self.buyer = nil
     self.sold_price = nil
     self.sold_at = nil
