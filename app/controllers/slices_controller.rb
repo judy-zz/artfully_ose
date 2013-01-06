6c3644d9c3e2d7964ef2f7580e1260f0cc475159
Gary Moore <gary.moore@gmail.com>
2013-01-05 21:18:16 -0500

    (OSE) Live slicing

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -5,8 +5,13 @@ class SlicesController < ArtfullyOseController
   end
 
   def data
+    data = []
+    @statement.payment_method_rows.values.each do |row|
+      data << Slice.new(row.payment_method, ["#AA0000","#00AA00","#000099"].sample, row.tickets) unless row.tickets == 0
+    end
+
     respond_to do |format|
-      format.json { render :partial => "data.json" }
+      format.json { render :json => data }
     end
   end
 
