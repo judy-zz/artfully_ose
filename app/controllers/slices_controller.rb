a96ac0722715047db968fbc11b3869a64ac1c578
Gary Moore <gary.moore@gmail.com>
2013-01-07 16:13:57 -0500

    (OSE) About to try and clear out children

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -10,9 +10,10 @@ class SlicesController < ArtfullyOseController
     #   data << Slice.new(row.payment_method, ["#AA0000","#00AA00","#000099"].sample, row.tickets) unless row.tickets == 0
     # end
 
-    data = Slicer.slice(nil, nil, ["One", "A", ["Three", "Four"]], 0)
+    data = Slicer.slice(nil, nil, [["A","B"], ["1", "2", "3"], ["XX", "YY", "ZZ"]], 0)
 
     respond_to do |format|
+      #format.json { render :partial => "data.json" }
       format.json { render :json => Array.wrap(data) }
     end
   end
--- app/models/slicer.rb
+++ app/models/slicer.rb
@@ -18,45 +18,13 @@ class Slicer
 
     Array.wrap(blocks[current_depth]).each do |bk|
       current_slice = Slice.new(bk)
-      puts "#{"*" * current_depth}#{bk}"
+      puts "#{"*" * current_depth}#{bk} #{blocks.length} #{current_depth}"
+      if(blocks.length == current_depth+1)
+        current_slice.color = "#33DDFF"
+        current_slice.value = rand(10000)
+      end
       root_slice.children << slice(current_slice, tickets, blocks, current_depth+1)   
     end
     root_slice
   end
-
-  # def self.slice(root_slice, tickets, blocks, current_depth)
-  #   slices ||= []
-  #   Array.wrap(blocks[current_depth]).each do |bk|
-  #     current_slice = Slice.new(bk)
-  #     if (blocks.length == current_depth+1)
-  #       current_slice.value = rand(10000)
-  #       current_slice.color = "#339945"
-  #     end
-  #     current_slice.children = slice(current_slice, tickets, blocks, current_depth+1)   
-  #     slices << current_slice 
-  #   end
-  #   slices
-  # end
-
-  # def self.slice(root_slice, tickets, blocks, current_depth)  
-  #   root_slice ||= Slice.new
-
-  #   if(blocks.length == current_depth+1)
-  #     root_slice.children ||= []
-  #     blocks[current_depth].each do |bk|
-  #       puts "#{bk} setting value"
-  #       root_slice.children << Slice.new(bk, "#3399ED", rand(1000))
-  #     end
-
-  #     return root_slice
-  #   else
-  #     Array.wrap(blocks[current_depth]).each do |bk|
-  #       puts "#{bk} slicing children"
-  #       root_slice.name = bk
-  #       slice(root_slice, tickets, blocks, current_depth+1)
-  #     end
-  #   end 
-
-  #   root_slice
-  # end
 end
