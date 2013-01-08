0d9d2fae1fe09484aaae08834a9e8caea5575ce3
Gary Moore <gary.moore@gmail.com>
2013-01-07 21:43:03 -0500

    (OSE) Two procs

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -10,17 +10,32 @@ class SlicesController < ArtfullyOseController
     #   data << Slice.new(row.payment_method, ["#AA0000","#00AA00","#000099"].sample, row.tickets) unless row.tickets == 0
     # end
 
-    fifty = Proc.new do |tickets|
-      price_map = {:greater_than_fifty => 0, :less_than_fifty => 0}
-      tickets.each do |ticket|
-        puts ticket.price
-        ticket.price > 5000 ? price_map[:greater_than_fifty] = price_map[:greater_than_fifty] + 1 : price_map[:less_than_fifty] = price_map[:less_than_fifty] + 1
+    payment_method_proc = Proc.new do |items|
+      payment_method_map = {}
+      items.each do |item|
+        puts item.inspect
+        item_array = payment_method_map[item.order.payment_method]
+        item_array ||= []
+        item_array << item
+        payment_method_map[item.order.payment_method] = item_array
       end
-      price_map
+      payment_method_map
+    end
+
+    ticket_type_proc = Proc.new do |items|
+      ticket_type_map = {}
+      items.each do |item|
+        puts item.inspect
+        item_array = ticket_type_map[item.product.section.name]
+        item_array ||= []
+        item_array << item
+        ticket_type_map[item.product.section.name] = item_array
+      end
+      ticket_type_map
     end
 
     # NO LONGER ARRAY OF ARRAYS.  JUST ARRAY OF BLOCKS THAT YIELD ARRAYS [one, two, three]
-    data = Slicer.slice(nil, @show.tickets.all, [fifty], 0)
+    data = Slicer.slice(nil, @show.items.all, [ticket_type_proc], 0)
     #data = Slicer.slice(nil, nil, [["A","B"], ["1", "2", "3"], ["XX", "YY", "ZZ"]], 0)
 
     respond_to do |format|
--- app/models/slicer.rb
+++ app/models/slicer.rb
@@ -1,14 +1,14 @@
 class Slicer
 
   #
-  # Pass an array of tickets and an array of blocks to operate on those tickets
-  # The block should yeild a hasmap where keys are names and values are the array of tickets that apply, such as
-  # { :general_admission => tickets, :vip => tickets }
+  # Pass an array of things and an array of blocks to operate on those things
+  # The block should yeild a hasmap where keys are names and values are the array of things that apply, such as
+  # { :general_admission => things, :vip => things }
   #  
   # Blocks (for now) look like [["A", "B"], ["1", "2"], ["XX", "YY"]] simulating an array of arrays of keys
   #
 
-  def self.slice(root_slice, tickets, blocks, current_depth)
+  def self.slice(root_slice, things, blocks, current_depth)
     slices ||= []
 
     if root_slice.nil?
@@ -17,15 +17,17 @@ class Slicer
     root_slice.children ||= []
 
     unless blocks.length == current_depth
-      map = blocks[current_depth].call(tickets)
+      puts "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  CALLING"
+      map = blocks[current_depth].call(things)
+      puts "  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  DONE"
       Array.wrap(map.keys).each do |kee|
         current_slice = Slice.new(kee)
         puts "#{"*" * current_depth}#{kee} #{blocks.length} #{current_depth}"
         if(blocks.length == current_depth+1)
           current_slice.color = "#33DDFF"
-          current_slice.value = map[kee]
+          current_slice.value = map[kee].length
         end
-        root_slice.children << slice(current_slice, tickets, blocks, current_depth+1)
+        root_slice.children << slice(current_slice, map[kee], blocks, current_depth+1)
       end
     end
     root_slice.children = nil if root_slice.children.empty?
