f1ffd1aab452c0a42d0cfa7b6e30dabac88e233f
Gary Moore <gary.moore@gmail.com>
2013-01-08 13:21:49 -0500

    (OSE) WIP discounts report

---
+++ app/models/discounts_report.rb
@@ -0,0 +1,17 @@
+class DiscountsReport
+  attr_accessor :discount, :start_date, :end_date
+
+  def initialize(discount_code, start_date, end_date)
+    self.discount = Discount.where(:id => discount_code).includes(:tickets).first
+    self.discount ||= Discount.where(:code => discount_code).includes(:tickets).first
+  
+    self.start_date = start_date
+    self.end_date = end_date
+
+    @items = Item.includes(:order => :person, :show => :event)
+                 .where(:product_id => self.discount.tickets)
+                 .where('orders.created_at' => self.start_date..self.end_date)
+  end
+
+
+end
