90ebfaf87811f6c4a0f45dfd77e31d42eaee0023
Gary Moore <gary.moore@gmail.com>
2013-01-29 14:50:48 -0500

    (OSE) Don't dup uuid

--- app/models/show.rb
+++ app/models/show.rb
@@ -113,7 +113,7 @@ class Show < ActiveRecord::Base
   end
 
   def dup!
-    copy = Show.new(self.attributes.reject { |key, value| key == 'id' || key == 'state' })
+    copy = Show.new(self.attributes.reject { |key, value| key == 'id' || key == 'uuid' || key == 'state' })
     copy.event = self.event
     copy.datetime = copy.datetime + 1.day
     copy
